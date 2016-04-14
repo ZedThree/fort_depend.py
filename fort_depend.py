f8a780ea3df6b32c4a7ef7ce8e90ad5567513fe3
Peter Hill <zed.three@gmail.com>
2016-04-14 13:20:27 +0100

    Get USEs for each module individually
    Note that this commit also sorts the used modules

--- fort_depend.py
+++ fort_depend.py
@@ -2,6 +2,13 @@
 import os
 import re
 
+UNIT_REGEX = re.compile("^\s*(?P<unit_type>module|program)\s*(?P<modname>\w*)",
+                        re.IGNORECASE)
+END_REGEX = re.compile("^\s*end\s*(?P<unit_type>module|program)\s*(?P<modname>\w*)?",
+                       re.IGNORECASE)
+USE_REGEX = re.compile("^\s*use\s*(?P<moduse>\w*)\s*(, )?\s*(only)?\s*(:)?.*?$",
+                       re.IGNORECASE)
+
 
 class FortranProject(object):
     def __init__(self, files=None, macros={}, verbose=False):
@@ -45,7 +52,7 @@ class FortranProject(object):
                     print("\033[031mError\033[039m module \033[032m"+
                           mod+"\033[039m not defined in any files. Skipping...")
 
-            depends[source_file.filename] = graph
+            depends[source_file.filename] = sorted(graph)
 
         if verbose:
             for file_ in depends.keys():
@@ -98,34 +105,74 @@ class FortranFile(object):
         with open(self.filename, 'r') as f:
             contents = f.readlines()
 
-        self.modules = self.get_modules(contents)
-        self.uses = self.get_uses(contents, macros)
+        self.modules = self.get_modules(contents, macros)
+        self.uses = self.get_uses()
 
     def __repr__(self):
         return "FortranFile('{}')".format(self.filename)
 
-    def get_modules(self, contents):
+    def get_modules(self, contents, macros={}):
         """Return all the modules or programs that are in the file
 
         Args:
             contents: Contents of the source file
         """
 
-        p = re.compile("^\s*(?P<unit_type>module|program)\s*(?P<modname>\w*)",
-                       re.IGNORECASE).match
-
         contains = {}
+        found_units = []
+        starts = []
+        ends = []
 
-        for line in contents:
-            found = p(line)
-            if found:
-                name = found.group('modname')
-                contains[name] = FortranModule(unit_type=found.group('unit_type'),
+        for num, line in enumerate(contents):
+            unit = re.match(UNIT_REGEX, line)
+            end = re.match(END_REGEX, line)
+            if unit:
+                found_units.append(unit)
+                starts.append(num)
+            if end:
+                ends.append(num)
+
+        if found_units:
+            if len(found_units) != len(starts) != len(ends):
+                error_string = "Unmatched start/end of modules in {} ({} begins/{} ends)".format(
+                    self.filename, len(starts), len(ends))
+                raise ValueError(error_string)
+            for unit, start, end in zip(found_units, starts, ends):
+                name = unit.group('modname')
+                contains[name] = FortranModule(unit_type=unit.group('unit_type'),
                                                name=name,
-                                               filename=self.filename)
+                                               source_file=self,
+                                               text=(contents, start, end),
+                                               macros=macros)
 
         # Remove duplicates before returning
         return contains
+
+    def get_uses(self):
+
+        # flatten list of lists
+        return sorted(set([mod for module in self.modules.values()
+                           for mod in module.uses]))
+
+
+class FortranModule(object):
+    """A Fortran Module or Program
+
+    unit_type: 'module' or 'program'
+    name: Name of the module/program
+    filename: Name of the file containing the module/program
+    """
+    def __init__(self, unit_type, name, source_file, text, macros={}):
+        self.unit_type = unit_type.strip().lower()
+        self.name = name.strip().lower()
+        self.source_file = source_file
+        self.defined_at = text[1]
+        self.end = text[2]
+
+        self.uses = self.get_uses(text[0], macros)
+
+    def __repr__(self):
+        return "FortranModule({}, '{}', '{}')".format(self.unit_type, self.name, self.filename)
 
     def get_uses(self, contents, macros={}):
         """Return which modules are used in the file after expanding macros
@@ -134,13 +181,11 @@ class FortranFile(object):
             contents: Contents of the source file
             macros: Dict of preprocessor macros to be expanded
         """
-        p = re.compile("^\s*use\s*(?P<moduse>\w*)\s*(, )?\s*(only)?\s*(:)?.*?$",
-                       re.IGNORECASE).match
 
         uses = []
 
-        for line in contents:
-            found = p(line)
+        for line in contents[self.defined_at:self.end]:
+            found = re.match(USE_REGEX, line)
             if found:
                 uses.append(found.group('moduse').strip())
 
@@ -153,22 +198,6 @@ class FortranFile(object):
                     uniq_mods[i] = mod.replace(k, v)
 
         return uniq_mods
-
-
-class FortranModule(object):
-    """A Fortran Module or Program
-
-    unit_type: 'module' or 'program'
-    name: Name of the module/program
-    filename: Name of the file containing the module/program
-    """
-    def __init__(self, unit_type, name, filename):
-        self.unit_type = unit_type.strip().lower()
-        self.name = name.strip().lower()
-        self.filename = filename
-
-    def __repr__(self):
-        return "FortranModule({}, '{}', '{}')".format(self.unit_type, self.name, self.filename)
 
 
 # Definitions
