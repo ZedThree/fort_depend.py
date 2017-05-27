06fa2544fc07c45326c5cf0f9b96d7f702ec0251
Peter Hill <zed.three@gmail.com>
2017-05-27 21:24:04 +0100

    Make FortranFile tests more robust

--- tests/test_fortranfile.py
+++ tests/test_fortranfile.py
@@ -29,35 +29,93 @@ class TestEmptyFortranFile():
 class TestSimpleFortranFile():
     @classmethod
     def setup_class(cls):
-        cls.testfile = FortranFile(filename="moduleA.f90", readfile=False)
+        cls.testfile = FortranFile(filename="file.f90", readfile=False)
 
     def test_init(self):
-        assert self.testfile.filename == "moduleA.f90"
+        assert self.testfile.filename == "file.f90"
 
     def test_file_str(self):
-        assert str(self.testfile) == "moduleA.f90"
+        assert str(self.testfile) == "file.f90"
 
     def test_file_repr(self):
-        assert repr(self.testfile) == "FortranFile('moduleA.f90')"
+        assert repr(self.testfile) == "FortranFile('file.f90')"
 
     def test_get_uses(self):
         assert self.testfile.uses is None
         assert self.testfile.get_uses() == []
 
-    def test_get_modules(self):
-        contents = ['module modA\n', 'end module modA\n']
-        result = "{'modA': FortranModule(module, 'moda', 'moduleA.f90')}"
+    def test_get_single_module(self):
+        contents = ['module modA',
+                    'end module modA']
+        expected = {"modA": "FortranModule(module, 'moda', 'file.f90')"}
+
         assert self.testfile.modules is None
-        assert str(self.testfile.get_modules(contents)) == result
+        module_list = self.testfile.get_modules(contents)
+
+        for key, value in expected.items():
+            assert key in module_list
+            assert repr(module_list[key]) == value
+
+    def test_get_multiple_modules(self):
+        contents = ['module modA',
+                    'end module modA',
+                    'module modB',
+                    'end module modB']
+        expected = {"modA": "FortranModule(module, 'moda', 'file.f90')",
+                    "modB": "FortranModule(module, 'modb', 'file.f90')"}
+
+        assert self.testfile.modules is None
+        module_list = self.testfile.get_modules(contents)
+
+        for key, value in expected.items():
+            assert key in module_list
+            assert repr(module_list[key]) == value
+
+    def test_get_program(self):
+        contents = ['program progA', 'end program progA']
+        expected = {"progA": "FortranModule(program, 'proga', 'file.f90')"}
+
+        assert self.testfile.modules is None
+        module_list = self.testfile.get_modules(contents)
+
+        for key, value in expected.items():
+            assert key in module_list
+            assert repr(module_list[key]) == value
+
+    def test_get_program_and_multiple_modules(self):
+        contents = ['program progA',
+                    'end program progA',
+                    'module modA',
+                    'end module modA',
+                    'module modB',
+                    'end module modB']
+        expected = {"modA": "FortranModule(module, 'moda', 'file.f90')",
+                    "modB": "FortranModule(module, 'modb', 'file.f90')",
+                    "progA": "FortranModule(program, 'proga', 'file.f90')"}
+
+        assert self.testfile.modules is None
+        module_list = self.testfile.get_modules(contents)
+
+        for key, value in expected.items():
+            assert key in module_list
+            assert repr(module_list[key]) == value
 
 
 @pytest.mark.usefixtures("datadir")
 class TestReadFortranFile():
-    def test_uses(self):
+    def test_empty_uses(self):
         testfile = FortranFile(filename="moduleA.f90", readfile=True)
         assert testfile.uses == []
 
-    def test_modules(self):
+    def test_single_modules(self):
         testfile = FortranFile(filename="moduleA.f90", readfile=True)
         result = "{'modA': FortranModule(module, 'moda', 'moduleA.f90')}"
         assert str(testfile.modules) == result
+
+    def test_single_uses(self):
+        testfile = FortranFile(filename="moduleB.f90", readfile=True)
+        assert testfile.uses == ['modA']
+
+    def test_multiple_uses(self):
+        testfile = FortranFile(filename="moduleC.f90", readfile=True)
+        assert testfile.uses == ['modA', 'modB']
