a81b4580321fee8cfa7a3911bf55b2f21b168b26
Adam Jirasek <Adam.Jirasek@gmail.com>
2017-06-02 20:43:03 -0600

    updated depend files

--- data_util/depend.mk
+++ data_util/depend.mk
@@ -1,63 +1,90 @@
 # This file is generated automatically. DO NOT EDIT!
 
-fll_match_pattern.o :  \
+fll_duplicate.o :  \
+	fll_out.o \
+	fll_mv.o \
+	fll_type.o \
+	fll_mk.o
+
+fll_mk.o :  \
 	fll_out.o \
 	fll_type.o
+
+fll_mv.o :  \
+	fll_out.o \
+	fll_type.o \
+	fll_rm.o \
+	fll_stich.o
+
+fll_read.o :  \
+	fll_out.o \
+	fll_funct_prt.o \
+	fll_mv.o \
+	fll_type.o \
+	fll_mk.o
 
 fll_cat.o :  \
 	fll_out.o \
 	fll_type.o
 
-fll_read.o :  \
-	fll_mk.o \
-	fll_out.o \
-	fll_mv.o \
-	fll_type.o \
-	fll_funct_prt.o
-
-fll_out.o :  \
-	fll_type.o
-
-fll_nnodes.o :  \
-	fll_out.o \
-	fll_type.o \
-	fll_funct_prt.o
-
 fll_stich.o :  \
 	fll_out.o \
 	fll_type.o
 
-fll_locate.o :  \
+fll_read_ffa.o :  \
 	fll_out.o \
-	fll_type.o \
-	fll_funct_prt.o
-
-fll_funct_prt.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_mkdir.o :  \
-	fll_mk.o \
-	fll_type.o
-
-fll_rm.o :  \
-	fll_stich.o \
-	fll_out.o \
-	fll_type.o
-
-fll_duplicate.o :  \
-	fll_mk.o \
-	fll_out.o \
+	fll_funct_prt.o \
 	fll_mv.o \
-	fll_type.o
+	fll_type.o \
+	fll_mk.o
 
-fll_mv.o :  \
-	fll_stich.o \
+fll_deattach.o :  \
 	fll_out.o \
 	fll_type.o \
-	fll_rm.o
+	fll_stich.o
+
+fll_mods.o :  \
+	fll_funct_prt.o \
+	fll_read_ffa.o \
+	fll_getnbytes.o \
+	fll_duplicate.o \
+	fll_cp.o \
+	fll_getndata.o \
+	fll_rm.o \
+	fll_locate.o \
+	fll_type.o \
+	fll_write.o \
+	fll_mk.o \
+	fll_nnodes.o \
+	fll_deattach.o \
+	fll_match_pattern.o \
+	fll_write_ffa.o \
+	fll_read.o \
+	fll_mv.o \
+	fll_out.o \
+	fll_mkdir.o \
+	fll_sweep.o \
+	fll_cat.o \
+	fll_stich.o
 
 fll_write_ffa.o :  \
+	fll_out.o \
+	fll_type.o
+
+fll_getndata.o :  \
+	fll_locate.o \
+	fll_out.o \
+	fll_type.o
+
+fll_sweep.o :  \
+	fll_out.o \
+	fll_type.o \
+	fll_match_pattern.o
+
+fll_out.o :  \
+	fll_type.o
+
+fll_funct_prt.o :  \
 	fll_out.o \
 	fll_type.o
 
@@ -65,67 +92,40 @@ fll_write.o :  \
 	fll_out.o \
 	fll_type.o
 
-fll_mods.o :  \
-	fll_mv.o \
-	fll_locate.o \
-	fll_cat.o \
+fll_mkdir.o :  \
 	fll_type.o \
+	fll_mk.o
+
+fll_locate.o :  \
+	fll_out.o \
 	fll_funct_prt.o \
-	fll_stich.o \
-	fll_rm.o \
-	fll_mk.o \
-	fll_nnodes.o \
-	fll_write.o \
-	fll_getnbytes.o \
-	fll_duplicate.o \
-	fll_out.o \
-	fll_mkdir.o \
-	fll_deattach.o \
-	fll_write_ffa.o \
-	fll_getndata.o \
-	fll_read.o \
-	fll_cp.o \
-	fll_sweep.o \
-	fll_read_ffa.o \
-	fll_match_pattern.o
+	fll_type.o
 
-fll_getndata.o :  \
+fll_nnodes.o :  \
 	fll_out.o \
-	fll_type.o \
-	fll_locate.o
+	fll_funct_prt.o \
+	fll_type.o
 
-fll_sweep.o :  \
+fll_match_pattern.o :  \
 	fll_out.o \
-	fll_type.o \
-	fll_match_pattern.o
+	fll_type.o
 
 fll_type.o : 
+
+fll_cp.o :  \
+	fll_out.o \
+	fll_cat.o \
+	fll_type.o \
+	fll_duplicate.o \
+	fll_stich.o \
+	fll_mv.o \
+	fll_rm.o
+
+fll_rm.o :  \
+	fll_out.o \
+	fll_type.o \
+	fll_stich.o
 
 fll_getnbytes.o :  \
 	fll_out.o \
 	fll_type.o
-
-fll_read_ffa.o :  \
-	fll_mk.o \
-	fll_out.o \
-	fll_mv.o \
-	fll_type.o \
-	fll_funct_prt.o
-
-fll_mk.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_deattach.o :  \
-	fll_stich.o \
-	fll_out.o \
-	fll_type.o
-
-fll_cp.o :  \
-	fll_mv.o \
-	fll_cat.o \
-	fll_type.o \
-	fll_stich.o \
-	fll_duplicate.o \
-	fll_out.o \
-	fll_rm.o
--- examples/Example_MPI-IO/depend.mk
+++ examples/Example_MPI-IO/depend.mk
@@ -1,21 +1,21 @@
 # This file is generated automatically. DO NOT EDIT!
 
-Example_mpi-IO.o :  \
-	read_input.o \
-	../../mpi_util/fll_mpi_mods.o \
-	create_data_set.o \
-	save_individ_files.o \
-	save_root_part_file.o \
-	../../data_util/fll_mods.o
-
-save_individ_files.o :  \
-	../../data_util/fll_mods.o
-
-read_input.o :  \
+create_data_set.o :  \
 	../../data_util/fll_mods.o
 
 save_root_part_file.o :  \
 	../../data_util/fll_mods.o
 
-create_data_set.o :  \
+save_individ_files.o :  \
+	../../data_util/fll_mods.o
+
+Example_mpi-IO.o :  \
+	../../mpi_util/fll_mpi_mods.o \
+	save_individ_files.o \
+	create_data_set.o \
+	../../data_util/fll_mods.o \
+	read_input.o \
+	save_root_part_file.o
+
+read_input.o :  \
 	../../data_util/fll_mods.o
