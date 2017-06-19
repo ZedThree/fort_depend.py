50e4b50c85b9122269df85a587fed3c10b7e3ea8
Adam Jirasek <jiraseka@shrike.rnet>
2017-06-19 09:30:24 -0600

    add scan file function

---
+++ data_util/fll_scan_file.f90
@@ -0,0 +1,88 @@
+!
+!     Copyright (C) 2016  Adam Jirasek
+! 
+!     This program is free software: you can redistribute it and/or modify
+!     it under the terms of the GNU Lesser General Public License as published by
+!     the Free Software Foundation, either version 3 of the License, or
+!     (at your option) any later version.
+! 
+!     This program is distributed in the hope that it will be useful,
+!     but WITHOUT ANY WARRANTY; without even the implied warranty of
+!     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!     GNU Lesser General Public License for more details.
+! 
+!     You should have received a copy of the GNU Lesser General Public License
+!     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+!     
+!     contact: libm3l@gmail.com
+!
+!     Subroutine FLL_SCAN_FILE
+!
+!
+MODULE FLL_SCAN_FILE_M
+!
+! Description: Contains function fll_cp
+!
+! 
+! History:
+! Version   Date       Patch number  CLA     Comment
+! -------   --------   --------      ---     -------
+! 1.1       10/10/16                         Initial implementation
+!
+!
+! External Modules used
+!
+CONTAINS
+
+   FUNCTION FLL_SCAN_FILE(FILENAME,IOUNIT,FMT,FPAR) RESULT(PNODE)
+!
+! Description: Scan file, returns fll list with data names in file and their
+!       position in file in bytes
+!
+! External Modules used
+!     
+    USE FLL_TYPE_M
+    USE FLL_OUT_M
+    USE FLL_READ_M
+    IMPLICIT NONE
+!
+! Declarations
+!
+! Arguments description
+! Name         In/Out     Function
+! PNODE        Out        pointer to fll list with data in names in file
+! IOUNIR       In         pointer which is to be copied
+! FILENAME     In         Name of file
+! FMT          In         Format of file
+! FPAR         In/Out     structure containing function specific data
+!
+! Arguments declaration
+!
+   TYPE(DNODE), POINTER  :: PNODE
+   TYPE(FUNC_DATA_SET) :: FPAR
+   INTEGER :: IOUNIT
+   CHARACTER(LEN=FILE_NAME_LENGTH) :: FILENAME
+   CHARACTER :: FMT
+!
+! Local types
+!
+   LOGICAL :: OK
+
+   INQUIRE (FILE=TRIM(FILENAME), EXIST=OK)
+   IF(.NOT.OK) THEN
+      WRITE(FPAR%MESG,'(A,A)')' Read  - file does not exist ',TRIM(FILENAME)
+      CALL FLL_OUT('ALL',FPAR)
+      FPAR%SUCCESS = .FALSE.
+      PNODE => NULL()
+      RETURN
+   END IF
+!
+!  GET FILE STRUCTURE
+!
+   PNODE => FLL_READ(FILENAME,IOUNIT,FMT,FPAR,SCAN = 'Y')
+
+   RETURN 
+
+   END FUNCTION FLL_SCAN_FILE
+  
+END MODULE FLL_SCAN_FILE_M
