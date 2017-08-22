6e6d5f65bb90d9c4c9f7d59cee029f965c27b341
Adam Jirasek <jiraseka@shrike.rnet>
2017-08-22 08:00:10 -0600

    add type of integer

--- data_util/fll_read_ucd.f90
+++ data_util/fll_read_ucd.f90
@@ -58,7 +58,7 @@ MODULE FLL_READ_UCD_M
 !
 CONTAINS
 
-  FUNCTION FLL_READ_UCD(FILE,IOUNIT,FMT,FPAR) RESULT(PNODE)
+  FUNCTION FLL_READ_UCD(FILE,IOUNIT,FMT,ITYPE,FPAR) RESULT(PNODE)
 !
 ! Description: main function opening, reading and closing file
 !
@@ -85,6 +85,7 @@ CONTAINS
 ! IOUNIT       In         Number of unit
 ! FMT          In         Format - a,A ASCII, b,B - Binary, * not specified
 ! FPAR         In/Out     structure containing function specific data
+! ITYPE        In         type of integer (I) or (L)
 !
 ! Arguments declaration
 !
@@ -92,14 +93,13 @@ CONTAINS
    TYPE(DNODE), POINTER  :: PNODE
    TYPE(FUNC_DATA_SET) :: FPAR
    INTEGER :: IOUNIT
-   CHARACTER :: FMT
+   CHARACTER :: FMT,ITYPE
 !
 ! Local declarations
 !
    LOGICAL :: OK
    CHARACTER :: FMT_LOC
    INTEGER :: ISTAT
-   INTEGER(LINT) :: POS
 
    INQUIRE (FILE=TRIM(FILE), EXIST=OK)
    IF(.NOT.OK) THEN
@@ -148,8 +148,14 @@ CONTAINS
 !
 !   READ INITIAL NODE
 !
-    POS = 1
-    PNODE => READ_NODE_UCD(IOUNIT,FMT_LOC,FPAR)
+    SELECT CASE(ITYPE)
+      CASE('I','i')
+        PNODE => READ_NODE_UCD(IOUNIT,FMT_LOC,FPAR)
+      CASE('L','l')
+        PNODE => READ_NODE_UCD_L(IOUNIT,FMT_LOC,FPAR)
+      CASE DEFAULT
+        STOP'WRONG DATA FORMAT FOR UCD DATA SET'
+    END SELECT
     
     CLOSE(IOUNIT)
     IF(.NOT.ASSOCIATED(PNODE))THEN
@@ -277,6 +283,143 @@ CONTAINS
       PTMP => FLL_MK('bound_elem_type','S',1_LINT,1_LINT,FPAR)
       OK = FLL_MV(PTMP,PBELEM,FPAR)
       PTMP%S0 = 'tria3'
+      PTMP => FLL_MK('bound_elem_nodes','I',N3,3_LINT,FPAR)       
+      PTMP%I2 = I3(1:N3,:)
+      OK = FLL_MV(PTMP,PBELEM,FPAR)
+    END IF
+
+    IF(N4> 0)THEN
+
+      PBELEM  => FLL_MKDIR('belem_group', FPAR)
+      OK = FLL_MV(PBELEM,PBND,FPAR)
+      PTMP => FLL_MK('bound_elem_type','S',1_LINT,1_LINT,FPAR)
+      OK = FLL_MV(PTMP,PBELEM,FPAR)
+      PTMP%S0 = 'quad4'
+      PTMP => FLL_MK('bound_elem_nodes','I',N4,4_LINT,FPAR)       
+      PTMP%I2 = I4(1:N4,:)
+      OK = FLL_MV(PTMP,PBELEM,FPAR)
+
+    END IF
+
+
+    DEALLOCATE(I3,I4, STAT=ISTAT)
+     IF(ISTAT /= 0)STOP'ERROR DEALLOCATING MEMORY ==> fll_read_ucd ERR:306 '
+
+  END FUNCTION READ_NODE_UCD
+
+
+  FUNCTION READ_NODE_UCD_L(IOUNIT,FMT,FPAR) RESULT(PNODE)
+!
+! Description: Function reads a node
+!
+! 
+! History:
+! Version   Date       Patch number  CLA     Comment
+! -------   --------   --------      ---     -------
+! 1.1       10/10/16                         Initial implementation
+!
+!
+! External Modules used
+!  
+    USE FLL_TYPE_M
+    USE FLL_MK_M
+    USE FLL_MKDIR_M
+    USE FLL_MV_M
+    USE FLL_OUT_M
+    
+    IMPLICIT NONE  
+!
+! Declarations
+!
+! Arguments description
+! Name         In/Out     Function
+! PNODE        Out        Pointer to node
+! IOUNIT       In         Number of unit
+! FMT          In         Format - a,A ASCII, b,B - Binary
+! FPAR         In/Out     structure containing function specific data
+!
+! Arguments declaration
+!
+    TYPE(DNODE), POINTER :: PNODE
+    TYPE(FUNC_DATA_SET) :: FPAR
+    INTEGER :: IOUNIT
+    CHARACTER :: FMT
+!
+!  Local declarations
+!
+    TYPE(DNODE), POINTER :: PREG,PTMP,PBND,PBELEM
+    CHARACTER(LEN=NAME_LENGTH) :: TEXT
+    CHARACTER(LEN=6) :: ETYPE
+    INTEGER(LINT) :: NPTS,NELEM,A,B,C,I,N3,N4
+    REAL(RDOUBLE), POINTER :: COO(:,:)
+    INTEGER(LINT), ALLOCATABLE :: I4(:,:),I3(:,:)
+    INTEGER :: ISTAT
+    LOGICAL :: OK
+    CHARACTER(LEN = 200)TEXTLONG
+!
+!   disregard all lines starting with #  - 3 lines
+!
+    READ(IOUNIT, '(A)')TEXT
+    READ(IOUNIT, '(A)')TEXT
+    READ(IOUNIT, '(A)')TEXT
+!
+!  read mesh dimensions
+!
+    READ(IOUNIT,*)NPTS,NELEM,A,B,C
+!
+!  allocate memory for coordinates
+!
+    PNODE => FLL_MKDIR('unstr_grid_data', FPAR)
+    PREG  => FLL_MKDIR('region', FPAR)
+    OK = FLL_MV(PREG,PNODE,FPAR)
+    PBND  => FLL_MKDIR('boundary', FPAR)
+    OK = FLL_MV(PBND,PREG,FPAR)
+
+    PTMP => FLL_MK('coordinates','D',NPTS,3_LINT,FPAR)
+    OK = FLL_MV(PTMP,PREG,FPAR)    
+    COO => PTMP%D2
+
+    DO I=1,NPTS
+     READ(IOUNIT,*)A,COO(I,:)
+    END DO
+!
+!   add boundary name
+!
+    PTMP => FLL_MK('boundary_name','S',1_LINT,1_LINT,FPAR)
+    PTMP%S0 = 'wall'
+!
+!   read elements
+!
+    N3 = 0
+    N4 = 0
+!
+    ALLOCATE(I3(NELEM,3),I4(NELEM,4), STAT=ISTAT)
+     IF(ISTAT /= 0)STOP'ERROR ALLOCATING MEMORY ==> fll_read_ucd ERR:397 '
+
+
+    DO I=1,NELEM
+      READ(IOUNIT,'(A)')TEXTLONG
+      READ(TEXTLONG,*)A,B,ETYPE
+      SELECT CASE(ETYPE)
+      CASE('tri')
+        N3 = N3 + 1
+        READ(TEXTLONG, *)A,B,ETYPE,I3(N3,:)
+      CASE('quad')
+        N4 = N4 + 1
+        READ(TEXTLONG, *)A,B,ETYPE,I4(N4,:)
+      END SELECT
+
+    END DO
+!
+!  add it to the structure
+!   
+    IF(N3> 0)THEN
+
+      PBELEM  => FLL_MKDIR('belem_group', FPAR)
+      OK = FLL_MV(PBELEM,PBND,FPAR)
+      PTMP => FLL_MK('bound_elem_type','S',1_LINT,1_LINT,FPAR)
+      OK = FLL_MV(PTMP,PBELEM,FPAR)
+      PTMP%S0 = 'tria3'
       PTMP => FLL_MK('bound_elem_nodes','L',N3,3_LINT,FPAR)       
       PTMP%L2 = I3(1:N3,:)
       OK = FLL_MV(PTMP,PBELEM,FPAR)
@@ -297,9 +440,9 @@ CONTAINS
 
 
     DEALLOCATE(I3,I4, STAT=ISTAT)
-     IF(ISTAT /= 0)STOP'ERROR DEALLOCATING MEMORY ==> fll_read_ucd ERR:277 '
+     IF(ISTAT /= 0)STOP'ERROR DEALLOCATING MEMORY ==> fll_read_ucd ERR:443 '
 
-  END FUNCTION READ_NODE_UCD
+  END FUNCTION READ_NODE_UCD_L
 
   
 END MODULE FLL_READ_UCD_M
