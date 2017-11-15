ed1958c9fbcec8ec0eb02503e0ade67e5583df9c
Adam Jirasek <jiraseka@shrike.rnet>
2017-11-15 11:09:36 -0700

    read - add optional parameter ERRMSG

--- data_util/depend.mk
+++ data_util/depend.mk
@@ -1,7 +1,7 @@
 # This file is generated automatically by fort_depend.py. DO NOT EDIT!
 #
 #  Created by: jiraseka
-#  Date: 2017-09-08 19:44:16
+#  Date: 2017-11-15 18:08:21
 #
 
 fll_deattach.o :  \
@@ -58,13 +58,6 @@ fll_read.o :  \
 	fll_out.o \
 	fll_type.o
 
-fll_read_ucd.o :  \
-	fll_mkdir.o \
-	fll_mv.o \
-	fll_mk.o \
-	fll_out.o \
-	fll_type.o
-
 fll_cp.o :  \
 	fll_mv.o \
 	fll_cat.o \
@@ -102,15 +95,20 @@ fll_match_pattern.o :  \
 	fll_out.o \
 	fll_type.o
 
-fll_read_ffa.o :  \
-	fll_mv.o \
-	fll_funct_prt.o \
-	fll_mk.o \
+fll_rm.o :  \
+	fll_stich.o \
 	fll_out.o \
 	fll_type.o
 
 fll_locate.o :  \
 	fll_funct_prt.o \
+	fll_out.o \
+	fll_type.o
+
+fll_read_ffa.o :  \
+	fll_mv.o \
+	fll_funct_prt.o \
+	fll_mk.o \
 	fll_out.o \
 	fll_type.o
 
@@ -148,8 +146,10 @@ fll_mods.o :  \
 	fll_cp.o \
 	fll_read.o
 
-fll_rm.o :  \
-	fll_stich.o \
+fll_read_ucd.o :  \
+	fll_mkdir.o \
+	fll_mv.o \
+	fll_mk.o \
 	fll_out.o \
 	fll_type.o
 
