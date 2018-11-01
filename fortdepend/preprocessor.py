c769b66c546ad3fd9d04c0607506a49e9d3bff4a
Peter Hill <zed.three@gmail.com>
2018-11-01 10:06:36 +0000

    Fix super() call for py2.7

--- fortdepend/preprocessor.py
+++ fortdepend/preprocessor.py
@@ -4,7 +4,7 @@ import pcpp
 
 class FortranPreprocessor(pcpp.Preprocessor):
     def __init__(self):
-        super().__init__()
+        super(pcpp.Preprocessor, self).__init__()
 
     def parse_to_string_lines(self, text):
         with io.StringIO() as f:
