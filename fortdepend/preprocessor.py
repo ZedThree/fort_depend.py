b5541d313b79a7d0e0b417538eea641bd5268e3c
Peter Hill <peter.hill@york.ac.uk>
2023-07-05 10:00:17 +0100

    Add some docs and type-hints

--- fortdepend/preprocessor.py
+++ fortdepend/preprocessor.py
@@ -1,13 +1,17 @@
+"""Utilities for preprocessing Fortran"""
+
 import io
 import pcpp
 
 
 class FortranPreprocessor(pcpp.Preprocessor):
+    """Simple wrapper around `pcpp.Preprocessor` to write to string"""
     def __init__(self):
         super().__init__()
         self.add_path(".")
 
-    def parse_to_string(self, text, source):
+    def parse_to_string(self, text: str, source: str) -> str:
+        """Preprocess ``text`` straight to string"""
         with io.StringIO() as f:
             self.parse(text, source=source)
             self.write(f)
